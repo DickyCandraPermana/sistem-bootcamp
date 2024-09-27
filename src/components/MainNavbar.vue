3655c6847e5035e4f030528a2fe120988e40e74d
Dicky Candra Permana <candrapermanadicky@gmail.com>
2024-09-27 14:02:27 +0700

    Final

--- src/components/MainNavbar.vue
+++ src/components/MainNavbar.vue
@@ -26,26 +26,33 @@ const toggleMenu = () => {
       Contact Us
     </button>
     <button @click="toggleMenu" class="flex flex-col gap-1 py-2 font-bold text-white md:hidden font-sen">
-      <div class="h-[2px] w-[20px] bg-black"></div>
-      <div class="h-[2px] w-[20px] bg-black"></div>
-      <div class="h-[2px] w-[20px] bg-black"></div>
+      <div :class="['h-[2px]', 'w-[20px]', 'bg-black', 'transition-all', 'duration-500', { 'rotate-45': isOpen }]">
+      </div>
+      <div :class="['h-[2px]', 'w-[20px]', 'bg-black', 'transition-all', 'duration-500', { 'hidden': isOpen }]"></div>
+      <div
+        :class="['h-[2px]', 'w-[20px]', 'bg-black', 'transition-all', 'duration-500', { '-rotate-45': isOpen, '-translate-y-2': isOpen }]">
+      </div>
     </button>
   </nav>
   <div v-if="isOpen" class="absolute z-10 w-full bg-slate-700">
     <ul>
       <li
         class="px-4 py-2 text-base font-normal leading-relaxed border-b text-slate-300 border-slate-600 font-inter hover:bg-slate-300 hover:text-slate-700">
-        <a href="">Home</a></li>
+        <a href="">Home</a>
+      </li>
       <li
         class="px-4 py-2 text-base font-normal leading-relaxed border-b text-slate-300 border-slate-600 font-inter hover:bg-slate-300 hover:text-slate-700">
-        <a href="">Blog</a></li>
+        <a href="">Blog</a>
+      </li>
       <li
         class="px-4 py-2 text-base font-normal leading-relaxed border-b text-slate-300 border-slate-600 font-inter hover:bg-slate-300 hover:text-slate-700">
-        <a href="">About</a></li>
+        <a href="">About</a>
+      </li>
       <li
         class="px-4 py-2 text-base font-normal leading-relaxed border-b text-slate-300 border-slate-600 font-inter hover:bg-slate-300 hover:text-slate-700">
-        <a href="">Contact Us</a></li>
+        <a href="">Contact Us</a>
+      </li>
     </ul>
   </div>
-  <div v-if="isOpen" class="absolute opacity-50 w-svw h-[100vh] bg-slate-800"></div>
+  <div v-if="isOpen" class="absolute opacity-50 w-svw h-[100vh] transition-all duration-500 bg-slate-800"></div>
 </template>
